8dc76b0bb341e9b8cea2bd86414822f1df078fc4
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-09-18 21:54:46 +0200

    result generator for past matches

--- royalbetsproject/royalbetsapp/views.py
+++ royalbetsproject/royalbetsapp/views.py
@@ -1,6 +1,38 @@
+from datetime import datetime
 from django.shortcuts import render, redirect
-from .models import Team, Fixture
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Team, Fixture
+import numpy as np
+
+
+def draw_results(home_odds, draw_odds, away_odds):
+    goal_perc_list = [7.7, 13.8, 23.1, 24.6, 12.4, 9.3, 5.1, 2.5, 1.1, 0.3, 0.1]
+    goal_prob_list = [x/100 for x in goal_perc_list]
+
+    home_win_prob = 1/home_odds
+    draw_prob = 1/draw_odds
+    away_win_prob = 1/away_odds
+    prob_sum = home_win_prob + draw_prob + away_win_prob
+
+    match_result = np.random.choice([1, 0, 2], p=[home_win_prob/prob_sum, draw_prob/prob_sum, away_win_prob/prob_sum])
+    match_goals = np.random.choice(np.arange(0, 11), p=goal_prob_list)
+
+    if not match_result:
+        if not match_goals:
+            winner_goals = loser_goals = 0
+        else:
+            winner_goals = loser_goals = match_goals // 2
+    else:
+        if not match_goals:
+            winner_goals = 1
+            loser_goals = 0
+        else:
+            winner_goals = np.random.randint(0, match_goals) + 1
+            loser_goals = match_goals - winner_goals
+
+    if match_result == 2:
+        return loser_goals, winner_goals
+    return winner_goals, loser_goals
 
 
 def table(request):
@@ -32,8 +64,9 @@ def matches(request):
 
     fixtures_sorted = []
     for fx in fixtures:
-        fixtures_sorted.append(fx)
-    n = len(fixtures)
+        if not fx.played:
+            fixtures_sorted.append(fx)
+    n = len(fixtures_sorted)
     for i in range(n-1):
         for j in range(0, n-i-1):
             if fixtures_sorted and fixtures_sorted[j].date > fixtures_sorted[j+1].date:
@@ -60,6 +93,17 @@ def results(request):
 
     fixtures_sorted = []
     for fx in fixtures:
+        if fx.date.timestamp() < datetime.now().timestamp() and not fx.played:
+            fx.played = True
+            fx.result_home, fx.result_away = draw_results(fx.odds_team_home, fx.odds_draw, fx.odds_team_away)
+
+            if fx.result_home > fx.result_away:
+                fx.winner_home = True
+            elif fx.result_home < fx.result_away:
+                fx.winner_away = True
+
+            fx.save()
+
         if fx.played:
             fixtures_sorted.append(fx)
     n = len(fixtures_sorted)
