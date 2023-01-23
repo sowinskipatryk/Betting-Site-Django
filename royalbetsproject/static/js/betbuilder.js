b1699a8635777cbddef53a984d005aea5109cdf2
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2023-01-24 00:41:01 +0100

    working betbuilder on the frontend

--- royalbetsproject/static/js/betbuilder.js
+++ royalbetsproject/static/js/betbuilder.js
@@ -1,9 +1,16 @@
 const betButtons = document.getElementsByClassName('betbutton');
 var betHeader = document.getElementById('betHeader');
 var chosenBets = document.getElementById('chosenBets');
-var userLogged = betHeader.dataset.logged == "true"
+var submitButton = $('input[type="submit"]');
+var userLogged = betHeader.dataset.logged == "true";
+var betSummary = $('#betSummary');
+var betOdds = $('#betOdds')[0];
+var betPrize = $('#betPrize')[0];
+var betStake = $('#betStake')[0];
+var stakeRange = $('#stakeRange');
 
 var betPicks = {}
+var odds = 0.0
 
 for (i=0; i<betButtons.length; i++) {
     betButtons[i].addEventListener('click', function(){
@@ -16,20 +23,55 @@ for (i=0; i<betButtons.length; i++) {
 
         if (betPicks[matchNum] !== undefined && betPicks[matchNum]['pick'] === data['pick']) {
             delete betPicks[matchNum];
+            odds /= parseFloat(odd);
         }
         else {
+            if (Object.keys(betPicks).length === 0) {
+            odds = parseFloat(odd);
+            }
+
+            else if (betPicks[matchNum] !== undefined)
+            {
+            odds /= parseFloat(betPicks[matchNum]['odd'])
+            odds *= parseFloat(odd)
+            }
+            else {
+            odds *= parseFloat(odd)
+            }
             betPicks[matchNum] = data
         }
 
         var betsNum = Object.keys(betPicks).length;
 
         if (userLogged) {
+
+            betStake.textContent = "$"+stakeRange[0].value;
+            betPrize.textContent = "$"+(Math.round(odds*stakeRange[0].value * 100) / 100)
+            stakeRange[0].oninput = function() {
+                betStake.textContent = "$"+this.value;
+                betPrize.textContent = "$"+(Math.round(odds*this.value * 100) / 100)
+            }
+
+            odds = Math.round(odds * 100) / 100
             if (betsNum == 1) {
-            betHeader.innerText = 'Single Bet'}
+            betHeader.innerText = 'Single Bet'
+            betOdds.innerText = odds.toString()
+            submitButton.removeClass('d-none')
+            betSummary.removeClass('d-none')
+            stakeRange.removeClass('d-none')
+            }
             else if (betsNum > 1) {
-            betHeader.innerText = 'Multi Bet'}
+            betHeader.innerText = 'Multi Bet'
+            betOdds.innerText = odds.toString()
+            submitButton.removeClass('d-none')
+            betSummary.removeClass('d-none')
+            stakeRange.removeClass('d-none')
+            }
             else {
-            betHeader.innerText = 'Dodaj pierwsze zdarzenie do kuponu'
+            betHeader.innerText = 'Add first event to the coupon'
+            submitButton.addClass('d-none');
+            betSummary.addClass('d-none');
+            stakeRange.addClass('d-none');
             }
 
             var string = "";
